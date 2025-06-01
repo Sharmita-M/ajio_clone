7aae13a673a81264a9d4a2cc9a2e332c0c3656ca
Sharmita-M <shreyamahapatra32@gmail.com>
2025-06-01 20:24:47 +0530

    pushing all the code first time

---
+++ admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   
+<div class="wrapper w-100 d-flex align-items-start justify-content-between bg-black h-100">
+
+    <nav class="sidebar position-relative">
+        <div class="sidebar-content w-100">
+            <div class="scrollbar-container w-100 h-100 d-flex justify-content-center flex-column">
+                <a href="" class="sidebar-brand d-flex justify-content-center align-items-center gap-1 py-2 text-center">
+                    <span class="align-middle me-5 text-uppercase text-white fw-bold fs-5 p-2">AJIO Panel</span>
+                </a>
+            </div>
+        </div>
+    </nav>
+</div>
+
+
+    <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+
+</body>
+</html>
